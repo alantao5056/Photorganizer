1b950faa8f35beddc8386b297db2dcd38332986c
alantao5056 <alantaolr@outlook.com>
2026-03-19 19:51:21 -0400

    Move FileTime to Models folder

--- src/App/MainWindow.xaml.cs
+++ src/App/MainWindow.xaml.cs
@@ -11,28 +11,10 @@ using System.Runtime.InteropServices;
 using Windows.Graphics;
 using Windows.Storage;
 using Windows.UI;
+using Alan.Photorganizer.App.Models;
 
 namespace Alan.Photorganizer.App
 {
-    public sealed class FileItem
-    {
-        public string Name { get; set; } = "";
-        public string Format { get; set; } = "";
-        public string CaptureTime { get; set; } = "";
-        public string DestFolder { get; set; } = "";
-        public bool HasExif { get; set; } = true;
-
-        public SolidColorBrush FormatFg { get; set; } = new();
-        public SolidColorBrush FormatBg { get; set; } = new();
-        public SolidColorBrush FormatBd { get; set; } = new();
-        public string DestIcon { get; set; } = "\uE8B7";
-        public SolidColorBrush DestFg { get; set; } = new();
-        public string StatusText { get; set; } = "Ready";
-        public SolidColorBrush StatusFg { get; set; } = new();
-        public SolidColorBrush StatusBg { get; set; } = new();
-        public SolidColorBrush StatusBd { get; set; } = new();
-    }
-
     public sealed partial class MainWindow : Window
     {
         [DllImport("user32.dll")]
---
+++ src/App/Models/FileItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alan.Photorganizer.App.Models
+{
+    public class FileItem
+    {
+        public string Name { get; set; } = "";
+        public string Format { get; set; } = "";
+        public string CaptureTime { get; set; } = "";
+        public string DestFolder { get; set; } = "";
+        public bool HasExif { get; set; } = true;
+
+        public SolidColorBrush FormatFg { get; set; } = new();
+        public SolidColorBrush FormatBg { get; set; } = new();
+        public SolidColorBrush FormatBd { get; set; } = new();
+        public string DestIcon { get; set; } = "\uE8B7";
+        public SolidColorBrush DestFg { get; set; } = new();
+        public string StatusText { get; set; } = "Ready";
+        public SolidColorBrush StatusFg { get; set; } = new();
+        public SolidColorBrush StatusBg { get; set; } = new();
+        public SolidColorBrush StatusBd { get; set; } = new();
+    }
+}
